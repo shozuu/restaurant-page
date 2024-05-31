729d299b0386c983c4e7e9949479a32bdde0d4cc
Allen Tan <tanallenbernard@gmail.com>
2024-05-31 13:28:02 +0800

    commit other files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # restaurant-page
-A multi-paged Restaurant website using WebPack to bundle files for The Odin Project's JavaScript Course
+A multi-page restaurant website created as part of The Odin Project's JavaScript course. This project utilizes Webpack to bundle JavaScript, CSS, and image files, demonstrating modular development and efficient build processes.
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
